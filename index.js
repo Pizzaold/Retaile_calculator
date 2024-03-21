42a871c3d8a39f0ea64e18335148d74d26dd567d
Jürmo Mihhailov <jurmoharak@gmail.com>
2024-03-21 13:59:02 +0200

    Me as user can see quantity and price [Delivers #187267194]

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let product = {
+    name: null,
+    price: 0,
+    quantity: 0
+};
+
+app.get('/', (req, res) => {
+    res.redirect('/product');
+});
+
+app.post('/product', (req, res) => {
+    const { name, price, quantity } = req.body;
+    product.name = name;
+    product.quantity = quantity;
+    product.price = price;
+    res.send(product);
+});
+
 app.get('/product', (req, res) => {
     res.send(product);
 });
