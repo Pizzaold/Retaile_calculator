bb566bc02333bc54b982bb225d7b4a1e51934cd4
Pizzaold <109032167+Pizzaold@users.noreply.github.com>
2024-04-03 09:55:17 +0300

    done and refaction (#1)
    Co-authored-by: Jürmo Mihhailov <jurmoharak@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const productRouter = require('./controllers/product');
 
 const app = express();
 const port = 3000;
@@ -7,25 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let product = {
-    name: null,
-    price: 0,
-    quantity: 0
+    quantity: 10,
+    price: 500
 };
+
+app.use('/product', productRouter);
 
 app.get('/', (req, res) => {
     res.redirect('/product');
-});
-
-app.post('/product', (req, res) => {
-    const { name, price, quantity } = req.body;
-    product.name = name;
-    product.quantity = quantity;
-    product.price = price;
-    res.send(product);
-});
-
-app.get('/product', (req, res) => {
-    res.send(product);
 });
 
 app.listen(port, () => {
