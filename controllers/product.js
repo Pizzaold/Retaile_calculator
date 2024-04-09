2580dd8c94f55a0392916f7518b8c9b628285d70
ThomasTheFknTrainEngine <marten.uiboupin@gmail.com>
2024-04-09 16:17:50 +0300

    Me as user can select between 3 more states

--- controllers/product.js
+++ controllers/product.js
@@ -10,6 +10,16 @@ const productRouter = (product) => {
         error: "price and quantity are required",
       });
     }
+    if (state && salesTax) {
+      return res.status(400).json({
+        error: "You can't specify both state and custom salesTax. Choose one.",
+      });
+    }
+    if (salesTax && (salesTax < 0 || salesTax > 1)) {
+      return res.status(400).json({
+        error: "salesTax must be a number between 0 and 1",
+      });
+    }
 
     const parsedDiscount = parseFloat(discount);
     const parsedPrice = parseFloat(price);
@@ -19,11 +29,19 @@ const productRouter = (product) => {
     const actualPrice = isNaN(parsedPrice) ? 1 : parsedPrice;
     const actualQuantity = isNaN(parsedQuantity) ? 1 : parsedQuantity;
 
+    const stateSalesTaxRates = {
+      "UK": 0.3,
+      "EE": 0.2,
+      "US": 0.4,
+      "AU": 0.5,
+      "CA": 0.6
+    };
+
     const newState = state;
 
-    const newSalesTax = parseFloat(salesTax) || 0.15;
+    const newSalesTax = parseFloat(stateSalesTaxRates[newState]) || salesTax || 0.15;
 
-    if (newState !== "EE" && newState !== "UK" && newState !== undefined) {
+    if (newState !== "EE" && newState !== "UK" && newState !== undefined && newState !== "US" && newState !== "AU" && newState !== "CA") {
       return res.status(400).json({
         error: "Invalid state",
       });
@@ -34,12 +52,6 @@ const productRouter = (product) => {
     product.discount = actualDiscount;
     product.salesTax = newSalesTax;
     product.state = newState;
-
-    if (newState === "UK") {
-      product.salesTax = 0.3;
-    } else if (newState === "EE") {
-      product.salesTax = 0.2;
-    };
 
     res.json(product);
   });
